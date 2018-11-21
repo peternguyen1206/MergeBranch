18d1373afb75e516c9fc005e123b34e7e0a98ca0
Peter Nguyen <peternguyen120693@gmail.com>
2018-11-21 18:43:27 +0900

    Add Animal Class and Modified Person file

---
+++ Animal.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self, name, numberOfLeg):
+        self.name = name
+        self.numberOfLeg = numberOfLeg
+    def showInfoOfAnimal(name, legs):
+        print(name, legs)
